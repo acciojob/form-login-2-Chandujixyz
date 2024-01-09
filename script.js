85391d1fc0589e267b43dc435177a6a171643e20
Chandan Gupta <112531619+Chandujixyz@users.noreply.github.com>
2024-01-09 09:01:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 //your JS code here. If required.
- const myform=document.getElementById("Myform");
- submit.addEventListener('click',event);
+//  const myform=document.getElementById("Myform");
+//  submit.addEventListener('click',event);
 
- function event(){
+//  function event(){
+//     var first=document.getElementById("first").value;
+//     var last=document.getElementById("last").value;
+//     var phone=document.getElementById("phone").value;
+//     var email=document.getElementById("email").value;
+// alert('First Name: ' + first+'\nLast Name: '+last+'\nPhone Number: '+phone+'\nEmail ID: '+email);
+//  };
+
+// **********************************
+const sub=document.getElementById("submit");
+sub.addEventListener('click',event);
+function event(){
     var first=document.getElementById("first").value;
     var last=document.getElementById("last").value;
     var phone=document.getElementById("phone").value;
     var email=document.getElementById("email").value;
-alert('First Name: ' + first+'\nLast Name: '+last+'\nPhone Number: '+phone+'\nEmail ID: '+email);
- };
+ alert('First Name: '+first+'\nLast Name: '+last+'\nPhone Number: '+phone+'\nEmail ID: '+email)
+}
