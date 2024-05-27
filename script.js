c180c2dee844b4ea9f4af37d8c2559667704f726
Chandan Gupta <chandugupta030797@gmail.com>
2024-05-27 15:11:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-//your JS code here. If required.
  
- const sub = document.getElementById("submit");
-sub.addEventListener('click', event);
-
-function event(e) {
-    e.preventDefault(); // Prevent the default form submission behavior
-
-    var first = document.getElementById("first").value;
-    var last = document.getElementById("last").value;
-    var phone = document.getElementById("phone").value;
-    var email = document.getElementById("email").value;
-
-    alert('First Name: ' + first + 'Last Name: ' + last + 'Phone Number: ' + phone + 'Email ID: ' + email);
-}
-
